ff7f08cb3d398f5e26ced35c39e16e5bdb8218b3
soupbread <96434847+soupbread@users.noreply.github.com>
2024-05-29 13:13:03 -0400

    New stuff

--- dinodino.py
+++ dinodino.py
@@ -1,35 +1,51 @@
 import pygame
 
-game_name = "Dino"
-game_icon = pygame.image.load("Media\dino.png")
-
-display_width = 300
-display_height = 300
-
 running = True
 
 pygame.init()
 
+# Display-related init
+# Window
+game_name = "Dino"
+game_icon = pygame.image.load('media\dino.png')
+
 pygame.display.set_caption(game_name)
 pygame.display.set_icon(game_icon)
 
+# Font
+font = pygame.font.Font('media\\font\GOUDYSTO.TTF', 50)
+
+# Screen
+display_width = 1080
+display_height = 720
+
 screen = pygame.display.set_mode((display_width, display_height))
-screen.fill("green")
+
+# Clock
 clock = pygame.time.Clock()
 
-surface = pygame.Surface((display_width-100, display_height-100))
-surface.fill("white")
+# Surfaces
+background = pygame.image.load('media\graphics\\backgrounds\grass.png')
+foreground = pygame.image.load('media\graphics\\backgrounds\grass1.png')
+player = pygame.image.load('media\graphics\player\mario-poster.png')
+text = font.render("Dino game",False, "#1a2b15")
 
+# Main loop
 while running:
+    # Exit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     
-    screen.fill("green")
-    surface.fill("white")
-    screen.blit(surface, (50,50))
+    # Surfaces init
+    screen.blit(background, (0,0))
+    screen.blit(foreground, (0,550))
+    screen.blit(player, (100, 400))
+    screen.blit(text, (300, 300))
 
+    # Refresh
     pygame.display.update()
-
+    
+    # Frame rate (ceiling)
     clock.tick(60)
