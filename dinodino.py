225f2c7ed57cf8633a240dc362dc210166f11d27
soupbread <96434847+soupbread@users.noreply.github.com>
2024-05-31 13:20:22 -0400

    New changes

--- dinodino.py
+++ dinodino.py
@@ -3,10 +3,11 @@ import pygame
 pygame.init()
 
 is_running = True
+is_playing = True
 
 # window properties
 pygame.display.set_caption("Dino")
-pygame.display.set_icon(pygame.image.load('media\dino.png'))
+pygame.display.set_icon(pygame.image.load('media/dino.png'))
 
 # display (screen)
 display = pygame.display.set_mode((800,400))
@@ -15,86 +16,102 @@ display = pygame.display.set_mode((800,400))
 clock = pygame.time.Clock()
 
 # surfaces
-background = pygame.image.load('media\graphics\environment\sky.png')
+background = pygame.image.load('media/graphics/environment/sky.png')
 
-ground = pygame.image.load('media\graphics\environment\ground.png')
+ground = pygame.image.load('media/graphics/environment/ground.png')
 ground_rect_1 = ground.get_rect(bottomleft=(0,400))
 ground_rect_2 = ground.get_rect(bottomleft=ground_rect_1.bottomright)
 
-foreground = pygame.image.load('media\graphics\environment\\foreground.png')
+foreground = pygame.image.load('media/graphics/environment/foreground.png')
 
 # characters
 # player
-player = pygame.image.load('media\graphics\characters\player\player.png')
+player = pygame.image.load('media/graphics/characters/player/player.png')
 player_rect = player.get_rect(bottomleft=(50, 300))
+player_grav = 0
 
 # enemies
-test_enemy = pygame.image.load('media\graphics\characters\player\player.png')
+test_enemy = pygame.image.load('media/graphics/characters/player/player.png')
 test_enemy_rect = test_enemy.get_rect(bottomleft=(700, 300))
+# test_enemy_grav = -20
 
 # general variables
-gravity = 10
+score = 0
 
 # title screen
 def title_screen():
     print("Displaying title screen")
 
+def player_jump():
+    global player_grav
+    global player_rect
+    
+    player_grav+=1
+    player_rect.y+=player_grav
+    if player_rect.bottom>=300:
+        player_rect.bottom=300
+
+def restart():
+    global test_enemy_rect
+    global ground_rect_1
+
+    test_enemy_rect.bottomleft=(700, 300)
+    ground_rect_1.bottomleft=(0,400)
+
 while is_running:
     # check whether window has been closed
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
-            pygame.quit()
-            is_running=False
-        if e.type == pygame.MOUSEBUTTONDOWN:
-            print("mouse pressed")
-            if e.button==1:
-                print("left mouse button pressed")
-        if e.type == pygame.KEYDOWN:
-            print("keydown")
-            if e.key==pygame.K_UP or e.key==pygame.K_w or e.key==pygame.K_SPACE:
-                print("JUMP")
-            if e.key==pygame.K_DOWN or e.key==pygame.K_s:
-                print("CROUCH")
-        if e.type==pygame.KEYUP:
-            print("keyup")
-    if not is_running:
-        break
+            is_running = False
+            is_playing = False
+        elif is_playing:
+            if e.type == pygame.MOUSEBUTTONDOWN:
+                if e.button==1 and player_rect.bottom==300:
+                    player_grav=-20
+            if e.type == pygame.KEYDOWN:
+                if (e.key==pygame.K_UP or e.key==pygame.K_w or e.key==pygame.K_SPACE) and player_rect.bottom==300:
+                    player_grav=-20
 
-    # or use exit()
+                if e.key==pygame.K_DOWN or e.key==pygame.K_s:
+                    print("CROUCH")
+        else:
+            if e.type == pygame.KEYDOWN and e.key==pygame.K_SPACE:
+                is_playing = True
+                restart()
+    
+    if is_playing:
+        
+        # all surfaces
+        display.blit(background, (0,0))
+        display.blit(ground, ground_rect_1)
+        display.blit(ground, ground_rect_2)
+        display.blit(player, player_rect)
+        display.blit(test_enemy, test_enemy_rect)
+        display.blit(foreground, (0, 250))
 
-    # surfaces
-    display.blit(background, (0,0))
-    display.blit(ground, ground_rect_1)
-    display.blit(ground, ground_rect_2)
-    display.blit(player, player_rect)
-    display.blit(test_enemy, test_enemy_rect)
-    display.blit(foreground, (0, 250))
+        # infinite scrolling platform
+        ground_rect_1.left-=5
+        ground_rect_2.left-=5
 
-    # player movement
-    # if pygame.key.get_pressed()[pygame.K_SPACE]:
-    #     print("JUMP")
+        if ground_rect_1.right<=800:
+            ground_rect_2.left=ground_rect_1.right
+        if ground_rect_2.right<=800:
+            ground_rect_1.left=ground_rect_2.right
 
-    # enemy movement
-    test_enemy_rect.left-=5
-    if test_enemy_rect.right<=0: test_enemy_rect.left=800
+        # player movement
+        player_jump()
 
-    # infinite scrolling platform
-    ground_rect_1.left-=5
-    ground_rect_2.left-=5
+        # enemy movement
+        test_enemy_rect.left-=5
+        if test_enemy_rect.right<=0: test_enemy_rect.left=800
 
-    if ground_rect_1.right<=800:
-        ground_rect_2.left=ground_rect_1.right
-    if ground_rect_2.right<=800:
-        ground_rect_1.left=ground_rect_2.right
+        # collision
+        if test_enemy_rect.colliderect(player_rect):
+            print("you died")
+            is_playing = False
 
-    # collision
-    if test_enemy_rect.colliderect(player_rect): collision_cnt+=1
-    else: collision_cnt=0
+        # update display
+        pygame.display.update()
 
-    if collision_cnt==1: print("COLLISION")
-
-    # update display
-    pygame.display.update()
-
-    # ceiling frame rate
-    clock.tick(60)
+        # ceiling frame rate
+        clock.tick(60)
